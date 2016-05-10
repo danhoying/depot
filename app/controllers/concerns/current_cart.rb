e3cd216120576d115398b2faf4cefe2ee95d2ec2
Dan Hoying <danhoying@gmail.com>
2016-05-10 15:23:12 -0700

    Add shopping cart button and functionality

--- app/controllers/concerns/current_cart.rb
+++ app/controllers/concerns/current_cart.rb
@@ -6,7 +6,7 @@ module CurrentCart
     def set_cart
       @cart = Cart.find(session[:cart_id])
     rescue ActiveRecord::RecordNotFound
-      @cart = Cat.create
+      @cart = Cart.create
       session[:cart_id] = @cart.id
     end
 end
